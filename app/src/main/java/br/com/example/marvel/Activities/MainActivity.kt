ea2604a7ce5eb6ed289054d0628d6d439ea4ec38
Laurent Lorena <laurent.lorena@dotz.com>
2019-05-24 18:06:02 -0300

    models

--- app/src/main/java/br/com/example/marvel/Activities/MainActivity.kt
+++ app/src/main/java/br/com/example/marvel/Activities/MainActivity.kt
@@ -3,11 +3,19 @@ package br.com.example.marvel.Activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import br.com.example.marvel.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val listView = listview
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    private fun getHeroes(): List<String> {
+        return listOf("superman", "spiderman","batman","hulk")
     }
 }
--- app/src/main/java/br/com/example/marvel/Env/URLs.kt
+++ app/src/main/java/br/com/example/marvel/Env/URLs.kt
@@ -1,6 +1,6 @@
 package br.com.example.marvel.Env
 
-public final class Env {
+class Env {
     companion object {
         const val baseUrl = "https://gateway.marvel.com/v1/public"
     }
